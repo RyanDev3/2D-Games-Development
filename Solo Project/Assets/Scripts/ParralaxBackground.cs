0c108fdae0d0bcfc970fa9fac4f4ad9cd819bd9e
Ryan <ryanmcgorie@gmail.com>
2025-04-30 11:02:08 +0100

    Looping Background

--- Solo Project/Assets/Scripts/ParralaxBackground.cs
+++ Solo Project/Assets/Scripts/ParralaxBackground.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class ParralaxBackground : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    public float backgroundSpeed;
+    public Renderer backgroundRenderer;
 
     // Update is called once per frame
     void Update()
     {
+        backgroundRenderer.material.mainTextureOffset += new Vector2(backgroundSpeed * Time.deltaTime, 0f);
         
     }
 }
