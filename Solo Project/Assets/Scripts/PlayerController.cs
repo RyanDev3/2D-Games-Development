bdf0034054235d8faf4c8ccdcaa073a945e68a74
Ryan <121602177+RyanDev2@users.noreply.github.com>
2025-05-14 13:58:18 +0100

    End Screen (Not Done)

--- Solo Project/Assets/Scripts/PlayerController.cs
+++ Solo Project/Assets/Scripts/PlayerController.cs
@@ -6,20 +6,15 @@ public class PlayerController : MonoBehaviour
     public float playerSpeed;
     private Rigidbody2D rb;
     private Vector2 playerDirection;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        //float directionY = Input.GetAxisRaw("Vertical");
-        //playerDirection = new Vector2(0, directionY).normalized;
 
         if (Input.GetKey(KeyCode.W))
         {
